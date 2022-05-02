dcf70a53ebb2dd7486ea1474a4ad958a6a2c2beb
Johnny Martinez <mrtnz.jn@gmail.com>
2022-05-02 11:23:47 -0500

    created tables_lec files and followed along with the lesson.

---
+++ tables_lec.sql
@@ -0,0 +1,25 @@
+USE codeup_test_db;
+
+CREATE TABLE IF NOT EXISTS users
+
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username    VARCHAR(100) NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    first_name  VARCHAR(255) NOT NULL,
+    last_name   VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS cats
+(
+    id    INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    breed VARCHAR(255) NOT NULL,
+    age   INT UNSIGNED NOT NULL,
+    namr  VARCHAR(255)
+);
+
+SHOW TABLES;
+
+DESCRIBE cats;
