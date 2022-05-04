bc616f04ea53a2485b3b4995b2ff315ecfd1dd40
Johnny Martinez <mrtnz.jn@gmail.com>
2022-05-04 06:45:39 -0500

    Completed all exercises in Order By Lesson.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -11,39 +11,31 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name;
 
 # ************ 3 **********
-SELECT last_name
+SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE 'E%'
-ORDER BY emp_no DESC;
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
 
 # ************ 4 **********
-SELECT last_name
+SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%'
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name ASC, first_name;
+
+# ************ 5 **********
+SELECT last_name, emp_no
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no;
+
+# ************ 6 **********
+
+SELECT last_name, emp_no
+FROM employees
+WHERE last_name LIKE '%e%'
 ORDER BY emp_no DESC;
 
-# ************ EXERCISE PART 2 *****************
-# ************ 1 & 2 **********
-SELECT first_name
+SELECT first_name, last_name
 FROM employees
-WHERE first_name = 'Irena' AND gender = 'M'
-   OR first_name = 'Vidya' AND gender = 'M'
-   OR first_name = 'Maya'  AND gender = 'M';
-
-# ************ 3 *************
-SELECT last_name
-FROM employees
-WHERE last_name LIKE 'E%'
-   OR last_name LIKE '%E';
-
-# ************ 4 *************
-SELECT last_name
-FROM employees
-WHERE last_name LIKE 'E%'
-  AND last_name LIKE '%E';
-
-# ************ 5 *************
-SELECT last_name
-FROM employees
-WHERE last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name;
